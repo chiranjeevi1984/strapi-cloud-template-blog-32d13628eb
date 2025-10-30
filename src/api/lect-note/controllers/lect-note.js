cbd4b0102e3b231ad6511f02fd502dd4a1291f0b
chiranjeevi1984 <kgchiranjeevi@gmail.com>
2025-10-30 17:54:38 +0530

    Updated the collection types

---
+++ src/api/lect-note/controllers/lect-note.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lect-note controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::lect-note.lect-note');
---
+++ src/api/lect-note/routes/lect-note.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lect-note router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::lect-note.lect-note');
